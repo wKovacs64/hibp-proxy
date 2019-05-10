af64023126d2b99264a577b28cb7f3b0242357e9
Justin Hall <justin.r.hall@gmail.com>
2019-05-10 12:12:39 -0600

    feat(pwnedpasswords): add Cache-Control response header

--- proxies/pwnedpasswords.js
+++ proxies/pwnedpasswords.js
@@ -1,6 +1,7 @@
 const fetch = require('node-fetch');
 
 module.exports = async (req, res) => {
+  res.setHeader('Cache-Control', 'public, max-age=2678400');
   res.end(
     await (await fetch(`https://api.pwnedpasswords.com${req.url}`, {
       headers: {
